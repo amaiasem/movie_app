ec51b895f661b3b2753fb239daad2ee39259e80a
Amaia <amaia.semper@gmail.com>
2021-05-02 00:46:42 +0200

    Create movies list styles: -Add card styles and hover effect -Add font awasome star rate. Add movie info: -Title and name -Release date -Average vote.

--- movie_app/movie-favs/src/components/MovieList/index.jsx
+++ movie_app/movie-favs/src/components/MovieList/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { bindActionCreators } from 'redux';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
 import loadMovies from '../../redux/actions/movieActionCreators';
@@ -8,25 +9,41 @@ import './index.scss';
 
 function MovieList({ movies, action }) {
   useEffect(() => {
-    // eslint-disable-next-line no-debugger
-    debugger;
     action.loadMovies();
   }, []);
 
   return (
-    <div>
-      <h1>Trending this week</h1>
+    <section className="movies__container">
+      <h2>Trending this week</h2>
       <div className="container__list">
         {
         movies && movies.map((movie) => (
-          <div className="movie-card">
+          <Link
+            href="/movie-detail"
+            className="movie__card"
+            params={{ movie }}
+          >
             <img src={`${posterSize.small}${movie.poster_path}`} alt={`Poster from ${movie.title}`} />
-            <p>{movie.title}</p>
-          </div>
+            <div className="movie__info">
+              {
+                movie.title
+                  ? <p className="movie__tile">{movie.title}</p>
+                  : <p className="movie__tile">{movie.name}</p>
+
+              }
+              <div className="movie__details">
+                <p>{movie.release_date}</p>
+                <div>
+                  <p>{movie.vote_average}</p>
+                  <i className="fas fa-star" />
+                </div>
+              </div>
+            </div>
+          </Link>
         ))
       }
       </div>
-    </div>
+    </section>
   );
 }
 
