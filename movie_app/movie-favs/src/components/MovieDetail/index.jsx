27d6ca89ef8438a772f2f5962b5f7e411bc0ee67
Amaia <amaia.semper@gmail.com>
2021-05-02 02:43:19 +0200

    Add info to movie detail and styles: -Add release date -Add media type -Add rate -Change image cover

--- movie_app/movie-favs/src/components/MovieDetail/index.jsx
+++ movie_app/movie-favs/src/components/MovieDetail/index.jsx
@@ -9,16 +9,28 @@ import './index.scss';
 const MovieDetail = ({ location }) => {
   const movie = location.props;
   return (
-    <div className="movie-detail">
+    <div className="movie__container">
       <img src={`${posterSize.large}${movie.backdrop_path}`} alt={`Poster from ${movie.title}`} />
-      <div className="movie-info">
+      <div className="movie-detail">
         {
             movie.title
               ? <h1>{movie.title}</h1>
               : <h1>{movie.name}</h1>
 
         }
-
+        <div className="movie-info">
+          <img src={`${posterSize.big}${movie.poster_path}`} alt={`Cover from ${movie.title}`} />
+          <div>
+            <h3 className="info__title">Overview</h3>
+            <p className="info__description">{movie.overview}</p>
+            <h3 className="info__title">Released</h3>
+            <p className="info__description">{movie.release_date}</p>
+            <h3 className="info__title">Media:</h3>
+            <p className="info__description">{(movie.media_type).toUpperCase()}</p>
+            <h3 className="info__title">Rate:</h3>
+            <p className="info__description">{movie.vote_average}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -29,7 +41,12 @@ MovieDetail.propTypes = {
     props: PropTypes.shape({
       title: PropTypes.string,
       name: PropTypes.string,
-      backdrop_path: PropTypes.string
+      backdrop_path: PropTypes.string,
+      poster_path: PropTypes.string,
+      overview: PropTypes.string,
+      release_date: PropTypes.string,
+      media_type: PropTypes.string,
+      vote_average: PropTypes.string
     }).isRequired
   }).isRequired
 };
--- movie_app/movie-favs/src/constants/posterURL.js
+++ movie_app/movie-favs/src/constants/posterURL.js
@@ -1,5 +1,5 @@
 const posterSize = {
-  big: 'https://image.tmdb.org/t/p/w500',
+  big: 'https://image.tmdb.org/t/p/w342',
   small: 'https://image.tmdb.org/t/p/w185',
   large: 'https://image.tmdb.org/t/p/w1280'
 
