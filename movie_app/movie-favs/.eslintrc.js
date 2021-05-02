5b30aff594beebb760f92c92051b1443aefca958
Amaia <amaia.semper@gmail.com>
2021-05-02 20:32:48 +0200

    Test action creators: loadAllmovies loadFavourites addFavourites deleteFavourites updateFavourites filterFavourites

--- movie_app/movie-favs/.eslintrc.js
+++ movie_app/movie-favs/.eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    jest: true
   },
   extends: [
     'plugin:react/recommended',
@@ -15,7 +16,8 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: [
-    'react'
+    'react',
+    'jest'
   ],
   rules: {
     'comma-dangle': [2, 'never'],
