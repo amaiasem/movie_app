17e2bef7ed8f599dc6c72f377269ea2be9f2d922
Amaia <amaia.semper@gmail.com>
2021-05-06 11:49:58 +0200

    Change babel and jest configuration

--- movie_app/movie-favs/jest.config.js
+++ movie_app/movie-favs/jest.config.js
@@ -1,11 +1,16 @@
+const { defaults } = require('jest-config')
+
 module.exports = {
   roots: ['<rootDir>/src'],
 
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.(js|jsx|ts|tsx)?$': 'babel-jest'
   },
 
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
 
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
+
+  transformIgnorePatterns: ['<rootDir>/node_modules/']
 }
